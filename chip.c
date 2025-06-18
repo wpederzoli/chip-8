6a04fc474b61feed2f3ee28d419928ab69a9fe09
William Pederzoli <wpederzoli@gmail.com>
2025-06-18 18:38:07 +0200

    Fetch decode loop (#2)
    * decode and log opcodes
    
    * formatting
    
    * Changed char and short for its uintX_t versions

--- chip.c
+++ chip.c
@@ -2,11 +2,10 @@
 #include "SDL3/SDL_error.h"
 #include "SDL3/SDL_log.h"
 #include "SDL3/SDL_stdinc.h"
-#include <stdlib.h>
 
-chip8_t* init_chip() {
-  chip8_t* chip = malloc(sizeof(chip8_t));
-  if(chip == NULL) {
+chip8_t *init_chip() {
+  chip8_t *chip = malloc(sizeof(chip8_t));
+  if (chip == NULL) {
     printf("Failed to allocate memory for the chip\n");
     return NULL;
   }
@@ -20,9 +19,9 @@ chip8_t* init_chip() {
   return chip;
 }
 
-int load_rom(chip8_t* chip, const char* path) {
-  FILE* file = fopen(path, "rb");
-  if(file == NULL) {
+int load_rom(chip8_t *chip, const char *path) {
+  FILE *file = fopen(path, "rb");
+  if (file == NULL) {
     printf("Cannot read file: %s", path);
     return -1;
   }
@@ -31,14 +30,34 @@ int load_rom(chip8_t* chip, const char* path) {
   long size = ftell(file);
   rewind(file);
 
-  if(size > ROM_MAX_S) {
-    printf("File is too large for this computer: %ld", size);
+  if (size > ROM_MAX_S) {
+    printf("File is too large for this computer: %ld\n", size);
     fclose(file);
     return -1;
   }
 
-  fread(&chip->memory[PROGRAMS_START_ADDR], 1, size, file) ;
+  fread(&chip->memory[PROGRAMS_START_ADDR], 1, size, file);
   fclose(file);
-  
+
   return 0;
+}
+
+void chip_step(chip8_t *chip) {
+  uint16_t opcode = (chip->memory[chip->PC] << 8) |
+                    chip->memory[chip->PC + 1]; // 2 byte opcodes
+  // opcodes look like 0xXYZW where:
+  //  X - Category
+  //  Y, Z, W - args (registers, other values, etc)...
+
+  uint8_t X = (opcode >> 12) & 0xF;
+  uint8_t Y = (opcode >> 8) & 0xF;
+  uint8_t Z = (opcode >> 4) & 0xF;
+  uint8_t W = opcode & 0xF;
+
+  // log for now
+  printf("PC: 0x%04X | Opcode: 0x%04X (X: 0x%X, Y: 0x%X, Z: 0x%X, W: 0x%X)\n",
+         chip->PC, opcode, X, Y, Z, W);
+
+  // Since it is 2 bytes sized we move forward 2 steps.
+  chip->PC += 2;
 }
--- chip.h
+++ chip.h
@@ -1,8 +1,8 @@
 #ifndef CHIP_H
 #define CHIP_H
 
-//4KB RAM
-#define RAM_S 4096 
+// 4KB RAM
+#define RAM_S 4096
 #define REGISTERS_S 16
 #define STACK_S 16
 
@@ -13,16 +13,19 @@
 
 #define ROM_MAX_S (0xFFF - 0x200 + 1)
 
+#include <stdlib.h>
+
 typedef struct {
-  unsigned char memory[RAM_S];
-  unsigned char V[REGISTERS_S]; // Registers V0 - VF
-  unsigned short I; //Register to store memory addresses 
-  unsigned short PC; //Program Counter
-  unsigned char SP; //Stack pointer
-  unsigned short stack[STACK_S]; 
+  uint8_t memory[RAM_S];
+  uint8_t V[REGISTERS_S]; // Registers V0 - VF
+  uint16_t I;             // Register to store memory addresses
+  uint16_t PC;            // Program Counter
+  uint8_t SP;             // Stack pointer
+  uint16_t stack[STACK_S];
 } chip8_t;
 
-chip8_t* init_chip();
-int load_rom(chip8_t* chip, const char* path);
+chip8_t *init_chip();
+int load_rom(chip8_t *chip, const char *path);
+void chip_step(chip8_t *chip);
 
 #endif
