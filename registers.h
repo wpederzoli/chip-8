ef6b6f78cd53046fd14a4f9357b7d0669904cdc7
William Pederzoli <wpederzoli@gmail.com>
2025-06-19 22:09:40 +0200

    Aritmetic and registers (#4)
    * Initial implementation for AND, OR, XOR, LD, and ADD
    
    * Corrections to implementation and other versions of LD and ADD implemented

--- registers.h
+++ registers.h
@@ -7,5 +7,21 @@
 #define RET 0x00EE
 // Jump to nnn address
 #define JP 0x1
+//Set Vx = kk
+#define LD_VX_KK 0x6
+//Set Vx = Vy
+#define LD_VX_VY 0x0
+//Set Vx = Vx + kk
+#define ADD 0x7
+//Logical ops
+#define LOGICAL 0x8 
+//Set Vx = Vx OR Vy
+#define OR 0x1
+//Set Vx = Vx & Vy
+#define AND 0x2
+//Set Vx = Vx XOR Vy
+#define XOR 0x3
+//Set Vx = Vx + Vy, set VF = carry.
+#define ADD_VX_VY 0x4
 
 #endif
