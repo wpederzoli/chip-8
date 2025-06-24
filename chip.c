34e1cd7e5557c72ff21846461e720ef439477224
William Pederzoli <wpederzoli@gmail.com>
2025-06-24 15:21:15 +0200

    Initial DRW implementation

--- chip.c
+++ chip.c
@@ -12,8 +12,11 @@ chip8_t *init_chip() {
 
   memset(chip->memory, 0, RAM_S);
   memset(chip->V, 0, REGISTERS_S);
+  memset(chip->gtx, 0, sizeof(chip->gtx));
+
   chip->I = 0;
   chip->SP = 0;
+  chip->DT = 0;
 
   chip->PC = PROGRAMS_START_ADDR;
   return chip;
@@ -55,6 +58,8 @@ void handle_opcode(chip8_t *chip, uint16_t op, SDL_Renderer *renderer) {
   switch (X) {
   case CLS:
     printf("Clear screen\n");
+    memset(chip->gtx, 0, sizeof(chip->gtx)); // Limpiar buffer interno
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     break;
   case RET:
@@ -69,12 +74,62 @@ void handle_opcode(chip8_t *chip, uint16_t op, SDL_Renderer *renderer) {
     return;
   }
   case LD_VX_KK: {
+    printf("LD_Vx_KK\n");
     chip->V[Y] = (Z << 4) | W;
     break;
   }
   case ADD: {
     uint8_t kk = (Z << 4) | W;
     chip->V[Y] = chip->V[Y] + kk;
+    break;
+  }
+  case DRW: {
+    printf("DRAW - X: %d, Y: %d, Rows: %d, I: 0x%04X\n", chip->V[Y], chip->V[Z],
+           W, chip->I);
+
+    uint8_t x = chip->V[Y] % 64;
+    uint8_t y = chip->V[Z] % 32;
+    chip->V[0xF] = 0;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    for (uint8_t row = 0; row < W; row++) {
+      uint8_t sprite_byte = chip->memory[chip->I + row];
+      for (uint8_t col = 0; col < 8; col++) {
+        if ((sprite_byte >> (7 - col)) & 1) {
+          printf("sprite draw\n");
+          uint8_t curr_x = (x + col) % 64;
+          uint8_t curr_y = (y + row) % 32;
+
+          if (chip->gtx[curr_y][curr_x]) {
+            chip->V[0xF] = 1;
+          }
+          chip->gtx[curr_y][curr_x] ^= 1;
+
+          // Dibuja el punto directamente (el escalado lo maneja SDL)
+          if (chip->gtx[curr_y][curr_x]) {
+            SDL_FRect rect = {curr_x *10.f, curr_y*10.f, 10.f, 10.f};
+            SDL_RenderFillRect(renderer, &rect);
+          }
+        }
+      }
+    }
+    break;
+  }
+  case LD_I_ADDR: {
+    uint16_t addr = (Y << 8) | (Z << 4) | W;
+    chip->I = addr;
+    break;
+  }
+  case SE: {
+    uint8_t kk = (Z << 4) | W;
+    if (chip->V[Y] == kk) {
+      chip->PC += 2;
+    }
+    break;
+  }
+  case LD_VX_DT: {
+    chip->V[Y] = chip->DT;
     break;
   }
   case LOGICAL: {
@@ -101,9 +156,13 @@ void handle_opcode(chip8_t *chip, uint16_t op, SDL_Renderer *renderer) {
       chip->V[0xF] = (sum > 0xFF) ? 1 : 0;
       break;
     }
+    default:
+      break;
     }
     break;
   }
+  default:
+    break;
   }
 
   // Since it is 2 bytes sized we move forward 2 steps.
@@ -111,8 +170,13 @@ void handle_opcode(chip8_t *chip, uint16_t op, SDL_Renderer *renderer) {
 }
 
 uint16_t chip_step(chip8_t *chip) {
+  if (chip->DT > 0) {
+    chip->DT--;
+  }
+
   uint16_t opcode = (chip->memory[chip->PC] << 8) |
                     chip->memory[chip->PC + 1]; // 2 byte opcodes
+  printf("PC: %04X | Opcode: %04X\n", chip->PC, opcode);
 
   return opcode;
 }
